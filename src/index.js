6fd649662c55b244aef221844010e09d1fddc358
Carlton-Ndolo <carltonmutua80@gmail.com>
2024-05-11 22:09:48 +0300

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<RouterProvider router={router} />);
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,37 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((response) => response.json())
+      .then((data) => setDirectors(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map((director) => (
+          <div key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Directors;
